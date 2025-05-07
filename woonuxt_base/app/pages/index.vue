68365953afe25252e396dbe668092c95442b6513
7onc1 <anton.demirev84@gmail.com>
2025-05-07 19:54:41 +0300

    Test Vercel

--- woonuxt_base/app/pages/index.vue
+++ woonuxt_base/app/pages/index.vue
@@ -46,7 +46,7 @@ useSeoMeta({
         <img src="/icons/box.svg" width="60" height="60" alt="Free Shipping" loading="lazy" />
         <div>
           <h3 class="text-xl font-semibold">Free Shipping</h3>
-          <p class="text-sm">Free shipping on order over €50</p>
+          <p class="text-sm">Free shipping on order over €51</p>
         </div>
       </div>
       <div class="flex items-center gap-8 p-8 bg-white rounded-lg">
